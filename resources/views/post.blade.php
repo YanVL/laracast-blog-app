8ff81cc04a1ddd961e18a0add12b35a70085ec7d
YanVL <yanvitor.gti@gmail.com>
2023-09-04 17:51:47 -0300

    starting the blog project

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+    <article>
+        <h1>{{ $post->title }}</h1>
+
+        <div>
+            <p>
+                {{ $post->body }}
+            </p>
+        </div>
+
+        <a href="/">Go Back</a>
+    </article>
+@endsection
