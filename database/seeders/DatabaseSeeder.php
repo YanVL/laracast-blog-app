967f91b1de5142e370421bba136d36aa4b99ffab
YanVL <yanvitor.gti@gmail.com>
2023-11-16 18:54:38 -0300

    27 - Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,52 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
-{
+{    
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $users = User::factory(10)->create();
+        $firstUser = $users->first();
+
+        $personal =Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        Post::create([
+            'user_id' => $firstUser->id,
+            'category_id' => $personal->id,
+            'title' => 'Personal',
+            'slug' => 'personal',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'lorem'
+        ]);
+
+        Post::create([
+            'user_id' => $firstUser->id,
+            'category_id' => $family->id,
+            'title' => 'Family',
+            'slug' => 'family',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => 'lorem'
+        ]);
     }
 }
